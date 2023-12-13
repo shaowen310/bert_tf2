d93b1513cc7b20a2db0542a51d02203de6f2189f
shaowen <shaowen310@outlook.com>
2023-12-13 10:45:52 +0000

    bert model done

--- bert.py
+++ bert.py
@@ -1,6 +1,44 @@
 import tensorflow as tf
 
 from embedding import Embedding, PositionEmbedding
+from transformer import TransformerEncoder
+
+
+def get_activation(activation_string):
+    """Maps a string to a Python function, e.g., "relu" => `tf.nn.relu`.
+
+    Args:
+      activation_string: String name of the activation function.
+
+    Returns:
+      A Python function corresponding to the activation function. If
+      `activation_string` is None, empty, or "linear", this will return None.
+      If `activation_string` is not a string, it will return `activation_string`.
+
+    Raises:
+      ValueError: The `activation_string` does not correspond to a known
+        activation.
+    """
+
+    # We assume that anything that"s not a string is already an activation
+    # function, so we just return it.
+    if not isinstance(activation_string, str):
+        return activation_string
+
+    if not activation_string:
+        return None
+
+    act = activation_string.lower()
+    if act == "linear":
+        return None
+    elif act == "relu":
+        return tf.nn.relu
+    elif act == "gelu":
+        return lambda features: tf.nn.gelu(features, approximate=True)
+    elif act == "tanh":
+        return tf.tanh
+    else:
+        raise ValueError("Unsupported activation: %s" % act)
 
 
 class BertEmbedding(tf.keras.layers.Layer):
@@ -46,7 +84,7 @@ class BertEmbedding(tf.keras.layers.Layer):
         )
 
         self.layer_norm = tf.keras.layers.LayerNormalization(axis=-1)
-        self.drop = tf.keras.layers.Dropout(self.dropout_rate)
+        self.dropout = tf.keras.layers.Dropout(self.dropout_rate)
 
     def call(self, inputs, token_type_ids=None):
         embedding_output = self.word_embedding(inputs)
@@ -63,9 +101,148 @@ class BertEmbedding(tf.keras.layers.Layer):
         embedding_output = self.position_embedding(embedding_output)
 
         embedding_output = self.layer_norm(embedding_output)
-        embedding_output = self.drop(embedding_output)
+        embedding_output = self.dropout(embedding_output)
 
         return embedding_output
 
-    def get_embedding_table(self):
+    def get_word_embedding_table(self):
         return self.word_embedding.get_embedding_table()
+
+
+class Bert(tf.keras.Model):
+    """BERT model ("Bidirectional Encoder Representations from Transformers").
+
+    Example usage:
+
+    ```python
+    # Already been converted into WordPiece token ids
+    input_ids = tf.constant([[31, 51, 99], [15, 5, 0]])
+    input_mask = tf.constant([[1, 1, 1], [1, 1, 0]])
+    token_type_ids = tf.constant([[0, 0, 1], [0, 2, 0]])
+
+    config = modeling.BertConfig(vocab_size=32000, hidden_size=512,
+      num_hidden_layers=8, num_attention_heads=6, intermediate_size=1024)
+
+    model = modeling.BertModel(config=config, is_training=True,
+      input_ids=input_ids, input_mask=input_mask, token_type_ids=token_type_ids)
+
+    label_embeddings = tf.get_variable(...)
+    pooled_output = model.get_pooled_output()
+    logits = tf.matmul(pooled_output, label_embeddings)
+    ...
+    ```
+    """
+
+    def __init__(
+        self,
+        vocab_size,
+        hidden_size=768,
+        type_vocab_size=16,
+        max_position_embeddings=512,
+        num_hidden_layers=12,
+        num_attention_heads=12,
+        intermediate_size=3072,
+        hidden_act="gelu",
+        hidden_dropout_prob=0.1,
+        attention_dropout_prob=0.1,
+        initializer_range=0.02,
+        **kwargs
+    ):
+        """Constructor for BertModel.
+
+        Args:
+          config: `BertConfig` instance.
+          is_training: bool. true for training model, false for eval model. Controls
+            whether dropout will be applied.
+          input_ids: int32 Tensor of shape [batch_size, seq_length].
+          input_mask: (optional) int32 Tensor of shape [batch_size, seq_length].
+          scope: (optional) variable scope. Defaults to "bert".
+
+        Raises:
+          ValueError: The config is invalid or one of the input tensor shapes
+            is invalid.
+        """
+        super().__init__(**kwargs)
+
+        self.vocab_size = vocab_size
+        self.hidden_size = hidden_size
+        self.max_position_embeddings = max_position_embeddings
+        self.type_vocab_size = type_vocab_size
+        self.num_hidden_layers = num_hidden_layers
+        self.num_attention_heads = num_attention_heads
+        self.intermediate_size = intermediate_size
+        self.hidden_act = hidden_act
+        self.hidden_dropout_prob = hidden_dropout_prob
+        self.attention_dropout_prob = attention_dropout_prob
+        self.initializer_range = initializer_range
+
+        ## init layers
+        self.bert_embedding = BertEmbedding(
+            self.vocab_size,
+            embedding_size=self.hidden_size,
+            type_vocab_size=self.type_vocab_size,
+            max_position_embeddings=self.max_position_embeddings,
+            dropout=self.hidden_dropout_prob,
+            initializer_range=self.initializer_range,
+            name="embeddings",
+        )
+
+        self.transformer_encoder = TransformerEncoder(
+            hidden_size=self.hidden_size,
+            num_hidden_layers=self.num_hidden_layers,
+            num_attention_heads=self.num_attention_heads,
+            intermediate_size=self.intermediate_size,
+            intermediate_act_fn=get_activation(self.hidden_act),
+            hidden_dropout=self.hidden_dropout_prob,
+            attention_dropout=self.attention_dropout_prob,
+            initializer_range=self.initializer_range,
+            name="encoder",
+        )
+
+        self.sequence_pool = tf.keras.layers.Dense(
+            self.hidden_size,
+            activation=tf.tanh,
+            kernel_initializer=tf.keras.initializers.TruncatedNormal(
+                stddev=self.initializer_range
+            ),
+            name="pooler",
+        )
+
+    def call(self, inputs, input_mask=None, token_type_ids=None):
+        """Constructor for BertModel.
+
+        Args:
+          config: `BertConfig` instance.
+          is_training: bool. true for training model, false for eval model. Controls
+            whether dropout will be applied.
+          input_ids: int32 Tensor of shape [batch_size, seq_length].
+          input_mask: (optional) int32 Tensor of shape [batch_size, seq_length].
+          token_type_ids: (optional) int32 Tensor of shape [batch_size, seq_length].
+
+        Raises:
+          ValueError: The config is invalid or one of the input tensor shapes
+            is invalid.
+        """
+        embedding_output = self.bert_embedding(inputs, token_type_ids=token_type_ids)
+
+        # Run the stacked transformer.
+        # `sequence_output` shape = [batch_size, seq_length, hidden_size].
+        all_encoder_layers = self.transformer_encoder(
+            embedding_output,
+            input_mask=input_mask,
+            do_return_all_layers=True,
+        )
+
+        sequence_output = all_encoder_layers[-1]
+
+        first_token_tensor = tf.squeeze(sequence_output[:, 0:1, :], axis=1)
+        pooled_output = self.sequence_pool(first_token_tensor)
+
+        return {
+            "sequence_output": sequence_output,
+            "pooled_output": pooled_output,
+            "all_encoder_layers": all_encoder_layers,
+        }
+
+    def get_word_embedding_table(self):
+        return self.bert_embedding.get_word_embedding_table()
