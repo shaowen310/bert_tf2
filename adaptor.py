33855930eefa8aabcfc8411619af3cbf1681a2db
shaowen <shaowen310@outlook.com>
2023-12-15 17:36:56 +0800

    cleanup

--- adaptor.py
+++ adaptor.py
@@ -22,22 +22,3 @@ class NextSentencePrediction(tf.keras.layers.Layer):
     def __call__(self, inputs):
         logits = self.next_sentence_logit(inputs)
         return logits
-
-        with tf.compat.v1.variable_scope("cls/seq_relationship"):
-            output_weights = tf.compat.v1.get_variable(
-                "output_weights",
-                shape=[2, bert_config.hidden_size],
-                initializer=modeling.create_initializer(bert_config.initializer_range),
-            )
-            output_bias = tf.compat.v1.get_variable(
-                "output_bias", shape=[2], initializer=tf.compat.v1.zeros_initializer()
-            )
-
-            logits = tf.matmul(input_tensor, output_weights, transpose_b=True)
-            logits = tf.nn.bias_add(logits, output_bias)
-            log_probs = tf.nn.log_softmax(logits, axis=-1)
-            labels = tf.reshape(labels, [-1])
-            one_hot_labels = tf.one_hot(labels, depth=2, dtype=tf.float32)
-            per_example_loss = -tf.reduce_sum(one_hot_labels * log_probs, axis=-1)
-            loss = tf.reduce_mean(per_example_loss)
-            return (loss, per_example_loss, log_probs)
